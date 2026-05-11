db97b5bd76ad7cf45033ddb3071c0338181c9d6d
dinhdat07 <dinhdatnguyen0710@gmail.com>
2026-05-11 11:41:21 +0700

    refactor(repositories): move implementations into impl package
    - Move repository implementations into repositories/impl.
    - Keep repository interfaces in repositories package.
    - Move related repository tests alongside their implementations.
    - Update imports to use the new implementation package.

--- internal/composer/repositories.go
+++ internal/composer/repositories.go
@@ -1,31 +1,31 @@
 package composer
 
 import (
-	"portal-system/internal/platform/storage"
+	repositories "portal-system/internal/repositories/impl"
 
 	"gorm.io/gorm"
 )
 
 type Repositories struct {
-	UserRepo       *storage.GormUserRepository
-	AuditLog       *storage.GormAuditLogRepository
-	TokenRepo      *storage.GormUserTokenRepository
-	RoleRepo       *storage.GormRoleRepository
-	PermissionRepo *storage.GormPermissionRepository
-	SessionRepo    *storage.GormAuthSessionRepository
-	RefreshRepo    *storage.GormRefreshTokenRepository
-	TxManager      *storage.GormTxManager
+	UserRepo       *repositories.GormUserRepository
+	AuditLog       *repositories.GormAuditLogRepository
+	TokenRepo      *repositories.GormUserTokenRepository
+	RoleRepo       *repositories.GormRoleRepository
+	PermissionRepo *repositories.GormPermissionRepository
+	SessionRepo    *repositories.GormAuthSessionRepository
+	RefreshRepo    *repositories.GormRefreshTokenRepository
+	TxManager      *repositories.GormTxManager
 }
 
 func newRepositories(db *gorm.DB) *Repositories {
 	return &Repositories{
-		UserRepo:       storage.NewGormUserRepository(db),
-		AuditLog:       storage.NewGormAuditLogRepository(db),
-		TokenRepo:      storage.NewGormUserTokenRepository(db),
-		RoleRepo:       storage.NewGormRoleRepository(db),
-		PermissionRepo: storage.NewGormPermissionRepository(db),
-		SessionRepo:    storage.NewGormAuthSessionRepository(db),
-		RefreshRepo:    storage.NewGormRefreshTokenRepository(db),
-		TxManager:      storage.NewGormTxManager(db),
+		UserRepo:       repositories.NewGormUserRepository(db),
+		AuditLog:       repositories.NewGormAuditLogRepository(db),
+		TokenRepo:      repositories.NewGormUserTokenRepository(db),
+		RoleRepo:       repositories.NewGormRoleRepository(db),
+		PermissionRepo: repositories.NewGormPermissionRepository(db),
+		SessionRepo:    repositories.NewGormAuthSessionRepository(db),
+		RefreshRepo:    repositories.NewGormRefreshTokenRepository(db),
+		TxManager:      repositories.NewGormTxManager(db),
 	}
 }
